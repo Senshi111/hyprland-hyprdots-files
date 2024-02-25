c08b97fcdf9483b2c164d32adcc88f46576e01a1
Bane <40372242+Senshi111@users.noreply.github.com>
2024-02-25 15:03:26 -0500

    Update install_pre.sh

--- Theme/Scripts/install_pre.sh
+++ Theme/Scripts/install_pre.sh
@@ -78,25 +78,25 @@ fi
 
 
 # pacman
-if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]
-    then
-    echo -e "\033[0;32m[PACMAN]\033[0m adding extra spice to pacman..."
+# if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]
+#     then
+#     echo -e "\033[0;32m[PACMAN]\033[0m adding extra spice to pacman..."
 
-    sudo cp /etc/pacman.conf /etc/pacman.conf.t2.bkp
-    sudo sed -i "/^#Color/c\Color\nILoveCandy
-    /^#VerbosePkgLists/c\VerbosePkgLists
-    /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
-    sudo sed -i '/^#\[multilib\]/,+1 s/^#//' /etc/pacman.conf
+#     sudo cp /etc/pacman.conf /etc/pacman.conf.t2.bkp
+#     sudo sed -i "/^#Color/c\Color\nILoveCandy
+#     /^#VerbosePkgLists/c\VerbosePkgLists
+#     /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
+#     sudo sed -i '/^#\[multilib\]/,+1 s/^#//' /etc/pacman.conf
 
-    #if [ $(grep -w "^\[xero_hypr\]" /etc/pacman.conf | wc -l) -eq 0 ] && [ $(grep "https://repos.xerolinux.xyz/xero_hypr/x86_64/" /etc/pacman.conf | wc -l) -eq 0 ]
-    #    then
-    #    echo "adding [xero_hypr] repo to pacman..."
-    #    echo -e "\n[xero_hypr]\nSigLevel = Required DatabaseOptional\nServer = https://repos.xerolinux.xyz/xero_hypr/x86_64/\n\n" | sudo tee -a /etc/pacman.conf
-    #fi
-    sudo pacman -Syyu
-    sudo pacman -Fy
+#     #if [ $(grep -w "^\[xero_hypr\]" /etc/pacman.conf | wc -l) -eq 0 ] && [ $(grep "https://repos.xerolinux.xyz/xero_hypr/x86_64/" /etc/pacman.conf | wc -l) -eq 0 ]
+#     #    then
+#     #    echo "adding [xero_hypr] repo to pacman..."
+#     #    echo -e "\n[xero_hypr]\nSigLevel = Required DatabaseOptional\nServer = https://repos.xerolinux.xyz/xero_hypr/x86_64/\n\n" | sudo tee -a /etc/pacman.conf
+#     #fi
+#     sudo pacman -Syyu
+#     sudo pacman -Fy
 
-else
-    echo -e "\033[0;33m[SKIP]\033[0m pacman is already configured..."
-fi
+# else
+#     echo -e "\033[0;33m[SKIP]\033[0m pacman is already configured..."
+# fi
 
